11c2e9350a019841ece7de17e2ea7459b460847d
Owusu Dennis Prah <45304000+til2to@users.noreply.github.com>
2022-10-31 23:58:51 +0000

    Added files via upload

---
+++ assignment002.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # My Jupyter Notebook on IBM Watson Studio
+
+# ### Owusu Dennis Prah
+# 
+# Data Scientist
+
+# ##### I'm interested in Data Science because data is as vital as information. Data Science is applicable in almost all sectors in life.
+
+# ### The code below is addition of x and y using the given variables
+
+# In[6]:
+
+
+x = 2
+y = 4
+x + y
+
+
+# Data science may include:
+# - ML
+# - Deep learning
+
+# Data science may include:
+# 1. ML
+# 2. Deep learning
+
+# ![Screenshot%202022-10-29%20at%2020.18.58.png](attachment:Screenshot%202022-10-29%20at%2020.18.58.png)
+
+# In[ ]:
+
+
+
+
